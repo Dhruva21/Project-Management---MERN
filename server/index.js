bd548556881d6887aa231c23cfff5f31347de7d6
dhruvaindavarapu <idhruva@horizon.csueastbay.edu>
2023-04-10 21:51:41 -0700

    changed the server port

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const schema = require('./schema/schema');
 
 const connectDB = require('./config/db');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
